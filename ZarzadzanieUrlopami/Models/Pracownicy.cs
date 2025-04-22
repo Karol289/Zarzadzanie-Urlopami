8993b90bb70ff81937e536871402936a8a256d48
Filip Kudła <114978661+takiktoS03@users.noreply.github.com>
2025-04-22 23:14:08 +0200

    walidacja do dodawania pracownikow

--- ZarzadzanieUrlopami/Models/Pracownicy.cs
+++ ZarzadzanieUrlopami/Models/Pracownicy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ZarzadzanieUrlopami.Models;
 
@@ -7,28 +8,40 @@ public partial class Pracownicy
 {
     public int IdPracownika { get; set; }
 
+    [Required(ErrorMessage = "Rola jest wymagana")]
     public int? IdRoli { get; set; }
 
+    [Required(ErrorMessage = "Imię jest wymagane")]
+    [StringLength(50, ErrorMessage = "Imię może mieć maksymalnie 50 znaków")]
     public string? Imie { get; set; }
 
+    [Required(ErrorMessage = "Nazwisko jest wymagane")]
+    [StringLength(50, ErrorMessage = "Nazwisko może mieć maksymalnie 50 znaków")]
     public string? Nazwisko { get; set; }
 
     public string? Plec { get; set; }
 
     public int? Wiek { get; set; }
 
+    [Required(ErrorMessage = "Email jest wymagany")]
+    [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu email")]
     public string? Mail { get; set; }
 
     public int? DzieciPonizej14 { get; set; }
 
+    [Required(ErrorMessage = "Data rozpoczęcia jest wymagana")]
     public DateOnly? DataRozpoczecia { get; set; }
 
     public int? StazPracyWDniach { get; set; }
 
     public string? Adres { get; set; }
 
+    [Required(ErrorMessage = "Telefon jest wymagany")]
+    [Phone(ErrorMessage = "Nieprawidłowy format numeru telefonu")]
     public string? Telefon { get; set; }
 
+    [Required(ErrorMessage = "Hasło jest wymagane")]
+    [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków")]
     public string? Haslo { get; set; }
 
     public virtual ICollection<DniRobocze> DniRoboczes { get; set; } = new List<DniRobocze>();
