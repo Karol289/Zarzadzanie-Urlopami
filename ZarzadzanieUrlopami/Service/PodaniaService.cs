956e9882ea32ca402eab12728e88416e41b457c8
juliasoltysik <julia.soltysik@gmail.com>
2025-05-06 20:01:03 +0200

    robocze podania

--- ZarzadzanieUrlopami/Service/PodaniaService.cs
+++ ZarzadzanieUrlopami/Service/PodaniaService.cs
@@ -1,6 +1,22 @@
-﻿namespace ZarzadzanieUrlopami.Service
+﻿
+using Microsoft.EntityFrameworkCore;
+using ZarzadzanieUrlopami.Data;
+using ZarzadzanieUrlopami.Models;
+
+namespace ZarzadzanieUrlopami.Service
 {
     public class PodaniaService
     {
+        private readonly UrlopyDbContext _context;
+
+        public PodaniaService(UrlopyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<TypyUrlopow>> GetTypyUrlopowAsync()
+        {
+            return await _context.TypyUrlopows.ToListAsync();
+        }
     }
+
 }
